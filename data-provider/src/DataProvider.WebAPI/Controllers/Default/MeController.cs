7273096679d9918cf8878d88224a7a9db66efba8
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-06-20 08:43:28 -0300

    implement me controoler

---
+++ data-provider/src/DataProvider.WebAPI/Controllers/Default/MeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataProvider.WebAPI.Controllers.Default
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInformation()
+            => Ok(new { name = "Investing Data Provider", version = "0.1.0" });
+    }
+}
