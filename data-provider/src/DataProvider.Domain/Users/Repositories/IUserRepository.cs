17f81b41c1e5a71052a5f42e43555311e19e864d
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-06-22 22:01:17 -0300

    implemente inteface for userRepository

---
+++ data-provider/src/DataProvider.Domain/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿using BaseEntity.Domain.Repositories;
+using DataProvider.Domain.Users.Models;
+
+namespace DataProvider.Domain.Users.Repositories
+{
+    public interface IUserRepository : IRepository<User> { }
+}
