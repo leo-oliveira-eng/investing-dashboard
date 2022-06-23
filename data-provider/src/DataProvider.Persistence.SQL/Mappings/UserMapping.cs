e86983cb4dfcec873ddfad00cf7d1455ed7ae0e6
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-06-23 08:26:40 -0300

    Implement user mapping

---
+++ data-provider/src/DataProvider.Persistence.SQL/Mappings/UserMapping.cs
@@ -0,0 +1,29 @@
+﻿using DataProvider.Domain.Users.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Valuables.Utils;
+
+namespace DataProvider.Persistence.SQL.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+
+            builder.HasKey(user => user.Id);
+
+            builder.Ignore(user => user.DomainEvents);
+
+            builder.OwnsOne(user => user.Email, email =>
+            {
+                email.Property(email => email.Address).HasColumnName(nameof(Email));
+            });
+
+            builder.OwnsOne(user => user.CPF, cpf =>
+            {
+                cpf.Property(cpf => cpf.Text).HasColumnName(nameof(CPF));
+            });
+        }
+    }
+}
