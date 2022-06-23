212ff0d8b987c66a9b6dcda313f24217dc992ba6
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-06-22 21:54:46 -0300

    implement context for sql database

---
+++ data-provider/src/DataProvider.Persistence.SQL/Context/Extensions/MediatorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using BaseEntity.Domain.Entities;
+using BaseEntity.Domain.Mediator.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataProvider.Persistence.SQL.Context.Extensions
+{
+    public static class MediatorExtensions
+    {
+        public static async Task PublishDomainEvents<TContext>(this IMediatorHandler mediator, TContext context) where TContext : DbContext
+        {
+            var domainEntities = context.ChangeTracker
+                .Entries<Entity>()
+                .Where(entry => entry.Entity?.DomainEvents.Count > 0);
+
+            var domainEvents = domainEntities
+                .SelectMany(entry => entry.Entity.DomainEvents)
+                .ToList();            
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => await mediator.PublishEvent(domainEvent));
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.ClearDomainEvents());
+
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+    }
+}
