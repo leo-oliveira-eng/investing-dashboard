a35439260d486f11985fd736317c9318e651a07a
leo-oliveira-eng <leo.oliveira.eng@outlook.com>
2025-11-24 22:47:42 -0300

    feat: basic refactor

---
+++ backend/src/01-InvestmentConsolidator.Api/Controllers/MeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestmentConsolidator.Api.Controllers;
+
+[ApiController, Route("echo")]
+public class MeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return Ok(new { name = "Investment Consolidator", version = "0.1.0" });
+    }
+}
--- data-provider/src/DataProvider.Domain/Users/Repositories/IUserRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿using BaseEntity.Domain.Repositories;
-using DataProvider.Domain.Users.Models;
-
-namespace DataProvider.Domain.Users.Repositories
-{
-    public interface IUserRepository : IRepository<User> { }
-}
